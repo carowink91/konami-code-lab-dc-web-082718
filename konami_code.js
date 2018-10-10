ff2ef146f3bf1e9b6dcf3f1264b48c7bb21f0e37
Angelica Winkler <carowink3@gmail.com>
2018-10-10 17:13:27 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,29 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  konamiListener();
+}
+
+function konamiListener() {
+let body = document.querySelector('body')
+ body.addEventListener('keydown', konami);
+}
+
+let index = 0;
+
+function konami(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      console.log("congrats");
+      window.alert("Congrats");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
